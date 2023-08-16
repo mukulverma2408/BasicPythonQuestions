0a45b981a96eb9dee34f1b21ec83b8209520a01b
mukulverma2408 <mukulverma2408@gmail.com>
2023-08-16 22:37:57 +0530

    Age Calculator second itetration

--- AgeCalculator.py
+++ AgeCalculator.py
@@ -1,24 +1,16 @@
 # Calculate the complete age of a person in years, months and days
-
 from datetime import datetime
 
-def ValidateDOB():
-    dt = input("Enter DOB in DD/MM/YYYY format : ")
-    dob = datetime.strptime(dt, "%d/%m/%Y")
-    dob = datetime.strftime(dob, "%d-%m-%Y")
-    current_date = datetime.now().strftime("%d-%m-%Y")
-    print("Current date is {}".format(current_date))
-    print("Entered date of birth is {}".format(dob))
-    if (current_date >= dob):
-        print("Valid Date, Moving Ahead")
-        #age = (current_date - dob)
-        print(type(current_date))
-        print(type(dob))
-        #print("Age is : {}".format(age))
-    else:
-        print("Date is not valid, please enter correct date")
+def validateDOB():
+    dob = input("Enter date of birth in DD/MM/YYYY format : ")
+    dobFormatted = datetime.strptime(dob, "%d/%m/%Y").date()
+    currdate = datetime.now().date()
+    if dobFormatted <= currdate:
+        print("DOB is valid, Moving Ahead!!!")
+        diff = currdate - dobFormatted
+        print("Age is : {}".format(diff))
+    elif dobFormatted > currdate:
+        print("Invalid DOB entered, please enter correct date")
         exit(1)
 
-ValidateDOB()
-
-
+validateDOB()
