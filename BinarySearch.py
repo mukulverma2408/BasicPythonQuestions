0feda022f33a796523111a3cf1e18fa2172595b0
mukulverma2408 <mukulverma2408@gmail.com>
2023-08-22 05:29:04 +0530

    Progg for 22nd Aug

--- BinarySearch.py
+++ BinarySearch.py
@@ -2,7 +2,27 @@ def binarysearch(arr, element):
     start = 0
     end = len(arr) - 1
     while start <= end:
-        mid = end // 2
+        mid = start + (end - start )// 2
+        print("Middle position is : {}".format(mid))
+        print("Middle Element is : {}".format(arr[mid]))
+        if arr[mid] > element:
+            end = mid - 1
+        elif arr[mid] < element:
+            start = mid + 1
+        elif arr[mid] == element:
+            print("========Element Found========")
+            return mid
+    print("Element Not found in array")
+    exit(1)
+
+arr = [8, 12, 16, 19, 23, 27, 29, 48, 71]
+print(binarysearch(arr, 48))
+
+
+
+
+
+
 
 
 
---
+++ BubbleSort.py
@@ -0,0 +1,18 @@
+def BubbleSort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = "False"
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = "True"
+        if swapped == "False":
+            break
+if __name__ == "__main__":
+    arr = [64, 34, 25, 12, 22, 11, 90]
+    BubbleSort(arr)
+    print(arr)
+
+
+
+
